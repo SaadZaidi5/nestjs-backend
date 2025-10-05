4cd084961835aecbfa107356e4d09c81a1f52ca9
SaadZaidi5 <144838628+SaadZaidi5@users.noreply.github.com>
2025-10-05 21:14:47 +0500

    changed main.ts file

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000','https://mathilda-horotelic-euclid.ngrok-free.dev'] // Your Next.js frontend URL
+    origin: [
+      'http://localhost:3000',
+      'https://mathilda-horotelic-euclid.ngrok-free.dev',
+    ], // Your Next.js frontend URL
     credentials: true,
   });
 
